906c15ecf2fd74700b5be4cca3dc14e7202b7ea2
Ashish Sigdel <a.asis.sigdel01@gmail.com>
2023-12-21 00:29:22 +0545

    fix minor error

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -64,7 +64,7 @@ const Skills = () => {
         ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Other_skill.map((image, index) => (
+        {Skill_data.map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
